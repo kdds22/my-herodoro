8b11a4d6c282b1aabd5f8a504a9ca24924afd147
kdds22 <klaus.dd.sa@gmail.com>
2021-03-03 16:36:08 -0300

    Fixed Bug

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,17 +7,15 @@ export default function Profile() {
     const { level } = useContext(ChallengesContext);
 
     return (
-        <ChallengesContextProvider>
-            <div className={style.profileContainer}>
-                <img src="https://github.com/kdds22.png" alt="Klaus Dellano" />
-                <div>
-                    <strong>Klaus Dellano</strong>
-                    <p>
-                        <img src="icons/level.svg" alt="Level" />
+        <div className={style.profileContainer}>
+            <img src="https://github.com/kdds22.png" alt="Klaus Dellano" />
+            <div>
+                <strong>Klaus Dellano</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
                         Level {level}
-                    </p>
-                </div>
+                </p>
             </div>
-        </ChallengesContextProvider>
+        </div>
     );
 }
